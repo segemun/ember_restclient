b5ffdf21d0d3e8f2996e5e5b5244244c1a68b153
Segemun <sergeymukhin21@gmail.com>
2017-02-17 23:58:12 +0000

    Fixed AuthenticatedRouteMixin beforeModel method redeclaration

--- app/routes/app.js
+++ app/routes/app.js
@@ -2,12 +2,13 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 const {
-    inject: { service }
+  inject: { service }
 } = Ember;
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-    currentUser: service(),
-    beforeModel(){
-       return this.get('currentUser').load();
-    }
+  currentUser: service(),
+  beforeModel(){
+    this._super(...arguments);
+    return this.get('currentUser').load();
+  }
 });
